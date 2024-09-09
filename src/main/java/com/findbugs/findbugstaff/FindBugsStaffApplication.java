d7438ddbfa638f59ec7d6f021d066219442ab422
D15M4S <122260287+D15M4S@users.noreply.github.com>
2024-09-09 16:48:33 +0900

    init: '초기 세팅'
    의존성 초기 세팅

---
+++ src/main/java/com/findbugs/findbugstaff/FindBugsStaffApplication.java
@@ -0,0 +1,13 @@
+package com.findbugs.findbugstaff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindBugsStaffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindBugsStaffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findbugs/findbugstaff/FindBugsStaffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findbugs.findbugstaff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindBugsStaffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
