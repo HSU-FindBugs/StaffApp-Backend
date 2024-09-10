3ada29bccf502f416eeda60453e6351607b80652
woody6624 <woody6624@gmail.com>
2024-09-10 17:04:58 +0900

    젠킨스 환경변수 넣기

--- src/test/java/com/findbugs/findbugstaff/FindBugsStaffApplicationTests.java
+++ src/test/java/com/findbugs/findbugstaff/FindBugsStaffApplicationTests.java
@@ -3,7 +3,7 @@ package com.findbugs.findbugstaff;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class FindBugsStaffApplicationTests {
 
 	@Test
