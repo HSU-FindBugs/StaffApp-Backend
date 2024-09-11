dea82d9e11b5003034971e42fb5290bf17e874bd
D15M4S <dismas5184@gmail.com>
2024-09-11 17:42:50 +0900

    feat: 공지 엔티티 구현

---
+++ src/main/java/com/findbugs/findbugstaff/domain/Notification.java
@@ -0,0 +1,32 @@
+package com.findbugs.findbugstaff.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SuperBuilder
+@Jacksonized
+@Getter
+public class Notification {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "notification_id")
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "staff_id")
+    private Staff staff;
+
+}
