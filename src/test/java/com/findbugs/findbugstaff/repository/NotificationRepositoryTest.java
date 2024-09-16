db27296586f8defe05acecc877d773f42f8af730
woody6624 <103871252+woody6624@users.noreply.github.com>
2024-09-16 20:37:32 +0900

    Update NotificationRepositoryTest.java
    @AutoConfigureTestDatabase

--- src/test/java/com/findbugs/findbugstaff/repository/NotificationRepositoryTest.java
+++ src/test/java/com/findbugs/findbugstaff/repository/NotificationRepositoryTest.java
@@ -19,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class NotificationRepositoryTest {
 
     @Autowired
